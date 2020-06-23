32a2af3bdbabdbcccfc42a12f458cb0f1584d602
bjningbo <bjningbo@corp.netease.com>
2020-06-23 11:05:52 +0800

    更新1.0.0版本

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+interface ConstructorProp{
+    width?: number,
+    height?: number,
+}
+interface FontStyle{
+    fontStyle?: string|number,
+    fontVariant?: string|number,
+    fontWeight?: string|number,
+    fontSize?: number,
+    lineHeight?: string|number,
+    fontFamily?: string,
+    left?: number,
+    top?: number,
+    maxWidth?: undefined|number,
+    content: string,
+    textAlign?: string,
+    textBaseline?: string,
+    direction?:string,
+    color?: string,
+    rotation?: number,
+    width?: undefined|number,
+}
+
+export = CreatePicture;
+
+declare class CreatePicture{
+    constructor(prop:ConstructorProp);
+    drawImage(image: any, dx: number, dy: number): void;
+    drawImage(image: any, dx: number, dy: number, dw: number, dh: number): void;
+    drawImage(image: any, sx: number, sy: number, sw: number|string, sh: number|string, dx: number, dy: number, dw: number, dh: number): void;
+    drawCirclePicture(image: any, dx: number, dy: number): void;
+    drawCirclePicture(image: any, dx: number, dy: number, dw: number, dh: number): void;
+    drawCirclePicture(image: any, sx: number, sy: number, sw: number|string, sh: number|string, dx: number, dy: number, dw: number, dh: number): void;
+    drawText(prop: FontStyle):number;
+    getPicture(type?: string, quality?: any);
+}
